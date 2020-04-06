e24f0ea0dd12ba9b544edf80c0890ba82426ecbf
Heesu Yun <Yunheesu@icloud.com>
2020-04-06 20:36:00 +0900

    Help Elon Musk - moving rockets to top and bottom and changing its images

--- Help Elon Musk/ViewController.swift
+++ Help Elon Musk/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var blastOffButton: UIButton!
+    
+    var currentImageNumber =  0
+    let numberOfImages = 3
+    var yAtLanding: CGFloat = 0 // dealing with individual numbers + initializing it by 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        yAtLanding = imageView.frame.origin.y // paying attention to where we are when we start off
     }
+    
+    @IBAction func rocketTapped(_ sender: UITapGestureRecognizer) { //tap gesture recognizer
+        currentImageNumber += 1
+        if currentImageNumber == numberOfImages {
+            currentImageNumber = 0
+        }
+        imageView.image = UIImage(named: "rocket\(currentImageNumber)")
+    }
+    @IBAction func blastOffButtonPressed(_ sender: UIButton) {
+        if imageView.frame.origin.y < 0 {
+            UIView.animate(withDuration: 2, animations: {self.imageView.frame.origin.y = self.yAtLanding}, completion: nil) //제자리로 돌아오게 하는것!
+            blastOffButton.setTitle("Blast Off", for: .normal)
+        }else{
+            UIView.animate(withDuration: 2, animations: {self.imageView.frame.origin.y = -self.imageView.frame.size.height}, completion: nil) //{}안에는 self가 들어가야한다
+            blastOffButton.setTitle("Return", for: .normal)
+        }
+        
+        
+}
+
 
 
 }
-
